21c8952458f7e0c900cc1ca15bde9aeb31dafbaf
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2024-11-25 22:37:14 +0100

    clip.go documentation improvements

--- player/clip.go
+++ player/clip.go
@@ -1,10 +1,12 @@
 package player
 
+// Clip defines an interface for audio playback sources.
 type Clip interface {
-	// Get the next AudioChunk of this clip. Returns nil if there is no more data.
+	// NextChunk retrieves the next audio chunk for playback.
+	// Returns the chunk and a boolean indicating if more chunks are available.
 	NextChunk() (*AudioChunk, bool)
 
-	// Abort playback of the current clip. Stop decoding or other connected processes.
+	// Stop aborts playback and stops any associated processes.
 	Stop()
 
 	// A string representation of the clip, for audio clips something like: [Artist] - [Title]
@@ -15,7 +17,12 @@ type Clip interface {
 }
 
 type AudioChunk = struct {
-	Left   []float32
-	Right  []float32
+	// Left channel samples.
+	Left []float32
+
+	// Right channel samples.
+	Right []float32
+
+	// Number of samples in this chunk (up to FRAMES_PER_BUFFER).
 	Length int
 }
