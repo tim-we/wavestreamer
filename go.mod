5a5fe8295c34ee326e671db57db77a06bcb99271
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2025-04-16 23:08:00 +0200

    Switch to go 1.24.2

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/tim-we/wavestreamer
 
-go 1.23.2
+go 1.24.2
 
 require github.com/gordonklaus/portaudio v0.0.0-20230709114228-aafa478834f5
 
--- player/decoder/file_decoding.go
+++ player/decoder/file_decoding.go
@@ -58,7 +58,10 @@ func (process *DecodingProcess) Close() {
 	process.stdout.Close()
 
 	if process.cmd.ProcessState == nil || !process.cmd.ProcessState.Exited() {
-		if err := process.cmd.Process.Kill(); err != nil && err != os.ErrProcessDone {
+		if process.cmd.Process == nil {
+			// On the Pi, for some reason, there is no process...
+			log.Printf("No OS process on %v", process.cmd)
+		} else if err := process.cmd.Process.Kill(); err != nil && err != os.ErrProcessDone {
 			log.Printf("Failed to kill process: %v", err)
 		}
 	}
