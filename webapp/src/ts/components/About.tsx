7470862efbfe471ea256b053912adf04b04b9f5d
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2025-05-25 22:15:27 +0200

    Show build date in webapp

--- webapp/src/ts/components/About.tsx
+++ webapp/src/ts/components/About.tsx
@@ -1,9 +1,14 @@
 import type { FunctionComponent } from "preact";
 
+declare const __BUILD_DATE__: string;
+
+const buildDate = new Date(__BUILD_DATE__);
+
 const About: FunctionComponent = () => (
   <footer id="about">
     The code for this project is&nbsp;
     <a href="https://github.com/tim-we/wavestreamer">available on GitHub</a>.
+    Build date {buildDate.toLocaleDateString()}.
   </footer>
 );
 export default About;
--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
+const now = new Date();
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
+	define: {
+		__BUILD_DATE__: JSON.stringify(now.toISOString())
+	},
 });
