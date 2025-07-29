a24212945873096bfe138e2df9800da9e628be1c
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2025-07-30 00:23:11 +0200

    Use custom highlighting API

--- webapp/src/ts/components/SongListModal.tsx
+++ webapp/src/ts/components/SongListModal.tsx
@@ -1,9 +1,13 @@
-import { type FunctionComponent, render } from "preact";
-import { useEffect, useRef, useState } from "preact/hooks";
+import { createContext, type FunctionComponent, render } from "preact";
+import { useContext, useEffect, useRef, useState } from "preact/hooks";
 import type WavestreamerApi from "../wavestreamer-api";
 import type { SearchResultEntry } from "../wavestreamer-api";
 
 const portal = document.getElementById("modal-portal")!;
+const highlight = new Highlight();
+const UserQueryContext = createContext("");
+const segmenter = new Intl.Segmenter(undefined, { granularity: "word" });
+CSS.highlights.set("search-results", highlight);
 
 export function show(radio: WavestreamerApi) {
   render(<SongListModal radio={radio} />, portal);
@@ -19,24 +23,26 @@ const SongListModal: FunctionComponent<SLMProps> = ({ radio }) => {
 
   useEffect(() => {
     const input = inputRef.current;
-
-    input.addEventListener("input", async () => {
+    const inputListener = async () => {
+      highlight.clear();
       const filter = input.value.trim();
       if (filter === "") {
         setClips([]);
       } else if (filter.length > 1) {
         setClips(await radio.search(filter));
       }
-    });
+    };
 
+    input.addEventListener("input", inputListener);
     input.focus();
 
     document.addEventListener("keydown", songListKeydownHandler);
 
     return () => {
       document.removeEventListener("keydown", songListKeydownHandler);
+      input.removeEventListener("input", inputListener);
     };
-  }, [inputRef.current?.value]);
+  }, []);
 
   return (
     <div
@@ -63,9 +69,11 @@ const SongListModal: FunctionComponent<SLMProps> = ({ radio }) => {
           />
         </div>
         <div id="song-list">
-          {clips.map((clip) => (
-            <Clip key={clip} radio={radio} clip={clip} />
-          ))}
+          <UserQueryContext.Provider value={inputRef.current?.value ?? ""}>
+            {clips.map((clip) => (
+              <Clip key={clip.id} radio={radio} clip={clip} />
+            ))}
+          </UserQueryContext.Provider>
         </div>
       </div>
     </div>
@@ -90,15 +98,49 @@ type ClipProps = {
 };
 
 const Clip: FunctionComponent<ClipProps> = ({ clip, radio }) => {
+  const userQuery = useContext(UserQueryContext);
+  const folderRef = useRef<HTMLElement>();
+  const filenameRef = useRef<HTMLElement>();
   const compontents = clip.name.split("/");
   const folder = compontents.slice(0, compontents.length - 1).join("/");
   const filename = compontents[compontents.length - 1];
 
+  useEffect(() => {
+    for (const { segment, isWordLike } of segmenter.segment(userQuery)) {
+      if (!isWordLike) {
+        continue;
+      }
+      let index = clip.name.indexOf(segment);
+      if (index < 0) {
+        continue;
+      }
+
+      const textNode =
+        index < folder.length - 1
+          ? folderRef.current.childNodes[0]
+          : filenameRef.current.childNodes[0];
+
+      if (index >= folder.length) {
+        index = textNode.textContent.indexOf(segment);
+      }
+
+      const range = new Range();
+      range.setStart(textNode, index);
+      range.setEnd(textNode, index + segment.length);
+
+      highlight.add(range);
+    }
+  }, [clip.name, userQuery]);
+
   return (
     <details class="song" name="clip">
       <summary>
-        {folder.length > 0 ? <span class="folder">{`${folder}/`}</span> : null}
-        <span class="file">{filename}</span>
+        {folder.length > 0 ? (
+          <span class="folder" ref={folderRef}>{`${folder}/`}</span>
+        ) : null}
+        <span class="file" ref={filenameRef}>
+          {filename}
+        </span>
       </summary>
       <div class="buttons">
         <button
