5846e3dc494d046e400180838d2e751057ad59cc
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2025-11-20 14:23:25 +0100

    Beep clip

---
+++ player/clips/beep_clip.go
@@ -0,0 +1,85 @@
+package clips
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/tim-we/wavestreamer/config"
+	"github.com/tim-we/wavestreamer/player"
+)
+
+const NUMBER_OF_CHUNKS = 10
+
+type BeepClip struct {
+	buffer chan *player.AudioChunk
+}
+
+func NewBeep() *BeepClip {
+	buffer := make(chan *player.AudioChunk, NUMBER_OF_CHUNKS)
+
+	go func() {
+		defer close(buffer)
+
+		for range NUMBER_OF_CHUNKS {
+			chunk := player.AudioChunk{
+				Left:  make([]float32, config.FRAMES_PER_BUFFER),
+				Right: make([]float32, config.FRAMES_PER_BUFFER),
+			}
+			generateWave(&chunk)
+			buffer <- &chunk
+		}
+	}()
+
+	return &BeepClip{buffer: buffer}
+}
+
+func (clip *BeepClip) NextChunk() (*player.AudioChunk, bool) {
+	chunk, hasMore := <-clip.buffer
+	return chunk, hasMore
+}
+
+func (clip *BeepClip) Stop() {
+	// This clip is so short, no point in providing custom stop logic.
+}
+
+func (clip *BeepClip) Duration() time.Duration {
+	durationInSeconds := (NUMBER_OF_CHUNKS * config.FRAMES_PER_BUFFER) / config.SAMPLE_RATE
+	return time.Duration(durationInSeconds) * time.Second
+}
+
+func (clip *BeepClip) Hidden() bool {
+	return true
+}
+
+func (clip *BeepClip) Duplicate() player.Clip {
+	return NewBeep()
+}
+
+const wavelengthInSamples = 64
+const bitmask = wavelengthInSamples - 1
+const slope = float32(2.0 / 32.0)
+const beepVolume = 0.2
+
+func (clip *BeepClip) Name() string {
+	frequency := config.SAMPLE_RATE / wavelengthInSamples
+	return fmt.Sprintf("Beep %v Hz", frequency)
+}
+
+func generateWave(chunk *player.AudioChunk) {
+	for i := range config.FRAMES_PER_BUFFER {
+		x := i & bitmask
+
+		var v float32
+
+		if x < 32 {
+			v = slope*float32(x) - 1
+		} else {
+			v = 1 - slope*float32(x)
+		}
+
+		v = beepVolume * v
+
+		chunk.Left[i] = v
+		chunk.Right[i] = v
+	}
+}
